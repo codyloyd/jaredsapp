b5e6fdcd9ac53efe55393bd41732514ed360737d
Cody Loyd <codyloyd@gmail.com>
2018-04-19 13:08:01 -0500

    pwa

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 
 module.exports = {
   entry: "./src/main.js",
@@ -58,6 +59,14 @@ if (process.env.NODE_ENV === "production") {
   module.exports.devtool = "#source-map";
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
+    // service worker caching
+    new SWPrecacheWebpackPlugin({
+      cacheId: 'jareds-app',
+      filename: 'service-worker.js',
+      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      minify: true,
+      stripPrefix: 'dist/'
+    }),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: '"production"'
